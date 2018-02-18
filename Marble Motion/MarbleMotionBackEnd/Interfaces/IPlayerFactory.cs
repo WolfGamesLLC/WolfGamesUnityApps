815d693e9a133a10cdb725ed00d8523b0ad76c1f
Dain Reinhart <dain@wolfgamesllc.com>
2018-02-18 08:50:00 -0500

    Added Player Factory

---
+++ Marble Motion/MarbleMotionBackEnd/Interfaces/IPlayerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace MarbleMotionBackEnd.Interfaces
+{
+    /// <summary>
+    /// Interface of a player factory
+    /// </summary>
+    public interface IPlayerFactory
+    {
+        IPlayerModel Model();
+    }
+}
---
+++ MarbleMotionXUnitTest/Factories/PlayerFactoryShould.cs
@@ -0,0 +1,34 @@
+﻿using MarbleMotionBackEnd.Factories;
+using MarbleMotionBackEnd.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarbleMotionXUnitTest.Factories
+{
+    /// <summary>
+    /// Test suite for the player factory
+    /// </summary>
+    public class PlayerFactoryShould
+    {
+        /// <summary>
+        /// Verify a player factory can be created
+        /// </summary>
+        [Fact]
+        public void CreatePlayerFactory()
+        {
+            Assert.NotNull(new PlayerFactory());
+        }
+
+        /// <summary>
+        /// Verify player models can be created
+        /// </summary>
+        [Fact]
+        public void CreatePlayerModelInstance()
+        {
+            var factory = new PlayerFactory();
+            Assert.IsAssignableFrom<IPlayerModel>(factory.Model());
+        }
+    }
+}
