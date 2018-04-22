408f5f84d3b3d7fb33715efcf34d22e450a0f502
Dain Reinhart <dain@wolfgamesllc.com>
2018-04-22 09:49:56 -0400

    Added player model json resource object

---
+++ MarbleMotionBackEnd/Models/PlayerModelResource.cs
@@ -0,0 +1,23 @@
+﻿namespace MarbleMotionBackEnd.Models
+{
+    /// <summary>
+    /// Player data model for the Marble Motion game
+    /// </summary>
+    public class PlayerModelResource : ApiResource
+    {
+        /// <summary>
+        /// The player's most recent recorded score
+        /// </summary>
+        public long Score { get; set; }
+
+        /// <summary>
+        /// The payer's character X position
+        /// </summary>
+        public int XPosition { get; set; }
+
+        /// <summary>
+        /// The payer's character Z position
+        /// </summary>
+        public int ZPosition { get; set; }
+    }
+}
