77f5e5a26d5ff97ae07e3a0ba18a1c8cbdec12eb
Dain Reinhart <dain@wolfgamesllc.com>
2018-04-29 06:21:28 -0400

    Now verify that a start button controllers uri option can be changed

--- MarbleMotionXUnitTest/Options/StartButtonControllerOptionsShould.cs
+++ MarbleMotionXUnitTest/Options/StartButtonControllerOptionsShould.cs
@@ -12,7 +12,7 @@ namespace MarbleMotionXUnitTest.Options
     public class StartButtonControllerOptionsShould
     {
         /// <summary>
-        /// Should use the local host http address by default
+        /// Verify the local host http address is used by default
         /// </summary>
         [Fact]
         public void UseLocalHttpByDefault()
@@ -20,6 +20,20 @@ namespace MarbleMotionXUnitTest.Options
             var _dut = new StartButtonControllerOptions();
 
             Assert.Equal(StartButtonControllerOptions.DefaultUri, _dut.Uri);
+        }
+
+        /// <summary>
+        /// Verify the Uri used can be changed
+        /// </summary>
+        [Fact]
+        public void GetAndSetUri()
+        {
+            var uri = "www.someuri.com";
+            var _dut = new StartButtonControllerOptions();
+
+            _dut.Uri = uri;
+
+            Assert.Equal(uri, _dut.Uri);
         }
     }
 }
