f6616fd23c5055922616a1e182b440dff8ef8e1b
Dain Reinhart <dain@wolfgamesllc.com>
2018-04-29 06:16:59 -0400

    Now configure start buttons with an option object

---
+++ MarbleMotionBackEnd/Options/StartButtonControllerOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MarbleMotionBackEnd.Options
+{
+    /// <summary>
+    /// Options to configure a <see cref="StartButtonController"/>
+    /// </summary>
+    public class StartButtonControllerOptions
+    {
+        /// <summary>
+        /// The default Uri used by the <see cref="StartButtonController"/> 
+        /// </summary>
+        public static readonly string DefaultUri = "https://localhost:44340/api/players/";
+
+        public StartButtonControllerOptions()
+        {
+            Uri = DefaultUri;
+        }
+
+        public string Uri { get; set; }
+    }
+}
---
+++ MarbleMotionXUnitTest/Options/StartButtonControllerOptionsShould.cs
@@ -0,0 +1,25 @@
+﻿using MarbleMotionBackEnd.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarbleMotionXUnitTest.Options
+{
+    /// <summary>
+    /// Verify the behaviors of a <see cref="StartButtonControllerOptions"/>
+    /// </summary>
+    public class StartButtonControllerOptionsShould
+    {
+        /// <summary>
+        /// Should use the local host http address by default
+        /// </summary>
+        [Fact]
+        public void UseLocalHttpByDefault()
+        {
+            var _dut = new StartButtonControllerOptions();
+
+            Assert.Equal(StartButtonControllerOptions.DefaultUri, _dut.Uri);
+        }
+    }
+}
