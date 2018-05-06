e45fb5e87c92ea801d16bbcd4883eff814a5e9ea
Dain Reinhart <dain@wolfgamesllc.com>
2018-05-06 09:31:50 -0400

    Now map WWW object errors to HttpStatusCodes

---
+++ Marble Motion/Assets/Scripts/Http/NonAsyncHttpClient.cs
@@ -0,0 +1,74 @@
+﻿using MarbleMotionBackEnd.Http;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Implement a non asyncronous http client
+/// </summary>
+public class NonAsyncHttpClient : MonoBehaviour
+{
+    private Uri uri;
+    private WWW client;
+
+    /// <summary>
+    /// Allow access to the response for testing purposes
+    /// until I can implement a work around that will let me 
+    /// unit test a MonoBehaviour effectively
+    /// </summary>
+    public WGHttpResponseMessage Response { get; set; }
+    
+    /// <summary>
+    /// Initilaize a syncronous http client
+    /// </summary>
+    public NonAsyncHttpClient()
+    {
+        Response = new WGHttpResponseMessage();
+    }
+
+    /// <summary>
+    /// Request a URI
+    /// </summary>
+    /// <param name="uri">The URI to request</param>
+    /// <returns>The resultant <see cref="HttpResponseMessage"/></returns>
+    public WGHttpResponseMessage Request(Uri uri)
+    {
+        this.uri = uri;
+
+        StartCoroutine(MakeRequest());
+
+        return Response;
+    }
+
+    /// <summary>
+    /// Convert a <see cref="WWW"/> error text to a <see cref="HttpStatusCode"/>
+    /// </summary>
+    /// <param name="errorText"></param>
+    public void SetStatusCode(string errorText)
+    {
+        Response.StatusCode = string.IsNullOrEmpty(errorText) ? HttpStatusCode.OK : HttpStatusCode.InternalServerError;
+    }
+
+    private IEnumerator MakeRequest()
+    {
+        client = new WWW(uri.ToString());
+        yield return client;
+
+        Response = new WGHttpResponseMessage();
+        if (! string.IsNullOrEmpty(client.error))
+        {
+            Response.StatusCode = HttpStatusCode.NotFound;
+            Response.ReasonPhrase = client.error;
+            Debug.Log("There was an error making the request: " + client.error);
+        }
+        else
+        {
+            Debug.Log(client.text);
+        }
+    }
+}
---
+++ MarbleMotionXUnitTest/Http/NonAsyncHttpClientShould.cs
@@ -0,0 +1,66 @@
+﻿using MarbleMotionBackEnd.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+using UnityEngine;
+
+namespace MarbleMotionXUnitTest.Http
+{
+    /// <summary>
+    /// Verify behavior of a <see cref="NonAsyncHttpClient"/> object
+    /// </summary>
+    public class NonAsyncHttpClientShould
+    {
+        private NonAsyncHttpClient dut;
+
+        public NonAsyncHttpClientShould()
+        {
+            dut = new NonAsyncHttpClient();
+        }
+
+        /// <summary>
+        /// Verify a request will be made of a remote system
+        /// </summary>
+        [Fact(Skip = "Until I can figure out how to properly unit test Unity internal methods")]
+        public void ReturnHttpResponseMessageFromRequest()
+        {
+            Assert.IsType<WGHttpResponseMessage>(dut.Request(new Uri("http://some.web.uri")));
+        }
+
+        /// <summary>
+        /// Verify the <see cref="HttpStatusCode"/> is set to <see cref="HttpStatusCode.OK"/>
+        /// if the <see cref="WWW"/> objects <see cref="WWW.error"/> property is null
+        /// </summary>
+        [Fact]
+        public void SetHttpStatusCodeOkWhenNull()
+        {
+            dut.SetStatusCode(null);
+            Assert.Equal(HttpStatusCode.OK, dut.Response.StatusCode);
+        }
+
+        /// <summary>
+        /// Verify the <see cref="HttpStatusCode"/> is set to <see cref="HttpStatusCode.OK"/>
+        /// if the <see cref="WWW"/> objects <see cref="WWW.error"/> property is empty
+        /// </summary>
+        [Fact]
+        public void SetHttpStatusCodeOkWhenEmpty()
+        {
+            dut.SetStatusCode("");
+            Assert.Equal(HttpStatusCode.OK, dut.Response.StatusCode);
+        }
+
+        /// <summary>
+        /// Verify the <see cref="HttpStatusCode"/> is set to <see cref="HttpStatusCode.InternalServerError"/>
+        /// if the <see cref="WWW"/> objects <see cref="WWW.error"/> property is not null or empty
+        /// </summary>
+        [Fact]
+        public void SetHttpStatusCodeOkWhenNotNullOrEmpty()
+        {
+            dut.SetStatusCode("Some unknown error text");
+            Assert.Equal(HttpStatusCode.InternalServerError, dut.Response.StatusCode);
+        }
+    }
+}
